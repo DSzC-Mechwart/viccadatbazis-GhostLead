f496664b12aa09f1df11136090a904fe46c1d978
GhostLead <jkomcjkomc@gmail.com>
2024-10-10 09:15:28 +0200

    majdnemkeszvan :) ( ofc some help ;) )

--- ViccAdatbazis/Models/Vicc.cs
+++ ViccAdatbazis/Models/Vicc.cs
@@ -2,15 +2,15 @@
 {
     public class Vicc
     {
-        public Vicc(int id, string tartalom, string feltolto, int tetszik, int nemTetszik)
-        {
-            Id = id;
-            Tartalom = tartalom;
-            Feltolto = feltolto;
-            Tetszik = tetszik;
-            NemTetszik = nemTetszik;
-            Aktiv = tetszik > nemTetszik ? true : false;
-        }
+        //public Vicc(int id, string tartalom, string feltolto, int tetszik, int nemTetszik)
+        //{
+        //    Id = id;
+        //    Tartalom = tartalom;
+        //    Feltolto = feltolto;
+        //    Tetszik = tetszik;
+        //    NemTetszik = nemTetszik;
+        //    Aktiv = tetszik > nemTetszik ? true : false;
+        //}
 
         public int Id { get; set; }
         public string Tartalom { get; set; }
